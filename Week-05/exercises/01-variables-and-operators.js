25ffb48eacb87a97affdbeb28b5a97805bc7300c
Barbimillan <barbii.millan@gmail.com>
2023-04-17 11:45:29 -0300

    BaSP-W05-2023: exercise 01-variables-and-operators

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,19 @@
+console.log("Exercise 01-variables-and-operators");
+
+console.log("Exercise 1a");
+var ageBarbara,ageMartina;
+ageBarbara = 29;
+ageMartina = 26;
+var totalages = ageBarbara + ageMartina;
+console.log(totalages);
+
+console.log("Exercise 1b");
+var firstname = "Martina";
+var surname = "Lopez";
+var fullname = firstname + " " + surname;
+console.log(fullname);
+
+console.log("Exercise 1c");
+var lengthfullname = "MartinaLopez";
+console.log(lengthfullname.length);
+
