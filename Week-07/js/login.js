a25f7c1bc895c0b8036e3a36324d602eb738e072
Barbimillan <barbii.millan@gmail.com>
2023-05-02 11:57:31 -0300

    BaSP-W07-2023: create modal success and error of Login form andcorrect some style of modal box

--- Week-07/js/login.js
+++ Week-07/js/login.js
@@ -49,34 +49,60 @@ var submitButton = document.getElementById("submit");
 var errorMessages = "";
 submitButton.onclick = function (event) {
   event.preventDefault();
-  var allInputsValid = true;
-  for (let i = 0; i < totalInputs.length; i++) {
+  for (var i = 0; i < totalInputs.length; i++) {
     if (!totalValidation[i]()) {
       totalInputs[i].style.border = "solid red 2px";
       divError[i].innerHTML = errorMessage[i];
       divError[i].style.display = "block";
-      errorMessages += errorMessage[i] + ", ";
-      allInputsValid = false;
     } else {
       totalInputs[i].style.border = "none";
       divError[i].style.display = "none";
     }
   }
-  if (!allInputsValid) {
-    return false;
-  } else {
-    var api = `https://api-rest-server.vercel.app/login?email=${emailInput.value}&password=${passwordInput.value}`;
-    fetch(api)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status === "success") {
-          alert(data);
-          console.log(data);
-        } else {
-          alert(data.msg);
+  fetch(
+    "https://api-rest-server.vercel.app/login?" +
+      "email=" +
+      emailInput.value +
+      "&password=" +
+      passwordInput.value
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        var valuesInput = "";
+        for (const property in data.data) {
+          valuesInput += property + ": " + data.data[property] + "\n";
         }
-      })
-      .catch((error) => console.log(error));
-    return true;
+        modal.style.display = "flex";
+        image.setAttribute("src", "../../Assets/Images/succes.png");
+        modalAlert.innerText = data.msg + "\n" + valuesInput;
+      } else if (data.errors) {
+        var errorInput = "";
+        data.errors.forEach((error) => {
+          errorInput += "\n" + error.msg;
+        });
+        throw new Error(errorInput);
+      } else if (data.msg) {
+        var backerror = "";
+        backerror += data.msg;
+        throw new Error(backerror);
+      }
+    })
+    .catch((error) => {
+      modal.style.display = "flex";
+      image.setAttribute("src", "../../Assets/Images/Error.png");
+      (modalAlert.innerText = error), error.msg;
+    });
+};
+var modal = document.getElementById("myModal");
+var span = document.querySelector(".close");
+var modalAlert = document.getElementById("content");
+var image = document.getElementById("image");
+span.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 };
